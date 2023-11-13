ffea5c2c71ba1cdd690cb12556192d49e4bb9ecd
YOUNTAEHEE <f_hyang@naver.com>
2023-11-13 18:43:52 +0900

    미션2

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ const colors = section.querySelectorAll('li');
 colors.forEach((color, index) => {
 	colors[index].addEventListener('click', () => {
 		if (colors[index].classList.contains('on')) return;
-		colors.forEach((color) => {
+		colors.forEach((color, idx) => {
 			color.classList.remove('on');
+			color.style.backgroundColor = '#333';
 		});
 		colors[index].classList.add('on');
+		colors[index].style.backgroundColor = colors[index].innerText;
 	});
 });
